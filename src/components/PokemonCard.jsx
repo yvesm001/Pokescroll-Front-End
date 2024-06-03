264f2236451ad99b8db004356edd544a2c8c8123
Yves Martinez <yves.oliver.m@gmail.com>
2024-06-03 11:46:09 -0400

    List component, pokemoncard, and details page in progress

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,26 @@
+// THIS COMPONENT IS EACH POKEMON RENDERED IN THE LIST POKEMON COMPONENT
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function PokemonCard({ pokemon }) {
+  return (
+    <div>
+    <Link to={"/PokemonDetails/" + pokemon.id}>
+      <img src={pokemon.official_artwork} />
+
+      <div>
+        <p>#{pokemon.id}</p>
+        <h1>{pokemon.name}</h1>
+
+        {/* Iterates through type and generates label for each */}
+        <div>
+          {pokemon.type.map((type) => {
+            return <p id={type.toLowerCase()}>{type}</p>;
+          })}
+        </div>
+      </div>
+      </Link>
+    </div>
+  );
+}
