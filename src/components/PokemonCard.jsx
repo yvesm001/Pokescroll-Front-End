3546fa086011808a32166b97ba4446660c63da75
Victor <victorortiz928@gmail.com>
2024-06-03 17:48:53 -0400

    Style Home Page & other pages

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,43 @@
 // THIS COMPONENT IS EACH POKEMON RENDERED IN THE LIST POKEMON COMPONENT
 
-import React from "react";
 import { Link } from "react-router-dom";
 
 export default function PokemonCard({ pokemon }) {
   return (
-    <div>
-    <Link to={"/PokemonDetails/" + pokemon.id}>
-      <img src={pokemon.official_artwork} />
-
-      <div>
-        <p>#{pokemon.id}</p>
-        <h1>{pokemon.name}</h1>
-
-        {/* Iterates through type and generates label for each */}
-        <div>
-          {pokemon.type.map((type) => {
-            return <p id={type.toLowerCase()}>{type}</p>;
-          })}
+    <div className="pokemonCard" style={{ padding: "8px" }}>
+      <div className="card" style={{ width: "18rem" }}>
+        <img to={"/PokemonDetails/:pokemonId"} src={pokemon.official_artwork} />
+        <div className="card-body">
+          <p>#{pokemon.id}</p>
+          <h1>{pokemon.name}</h1>
+          <Link to={"/PokemonDetails/" + pokemon.id} />
+          <span
+            className="name-type"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "10px",
+            }}
+          >
+            {pokemon.type.map((type) => {
+              return (
+                <p
+                  className="rounded-pill"
+                  style={{
+                    color: "white",
+                    padding: "8px",
+                    display: "flex",
+                  }}
+                  key={type}
+                  id={type.toLowerCase()}
+                >
+                  {type}
+                </p>
+              );
+            })}
+          </span>
         </div>
       </div>
-      </Link>
     </div>
   );
 }
