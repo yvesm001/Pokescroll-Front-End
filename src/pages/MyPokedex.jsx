910937f481f94a779af3a3e94fc77ae2ca7f7980
Victor <victorortiz928@gmail.com>
2024-06-04 01:16:52 -0400

    Fix MyPokedex Page

--- src/pages/MyPokedex.jsx
+++ src/pages/MyPokedex.jsx
@@ -36,40 +36,6 @@ const MyPokedex = () => {
     }
   };
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import NewPokedexEntry from '../components/NewPokedexEntry';
-import PokemonCard from '../components/PokemonCard';
-
-const MyPokedex = () => {
-  const [pokemon, setPokemon] = useState([]);
-
-  useEffect(() => {
-    const fetchPokemon = async () => {
-      try {
-        const response = await axios.get('https://pokemon-data.adaptable.app/pokedex');
-          setPokemon(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchPokemon();
-  }, []);
-
-  const handleAddPokemon = (newPokemon) => {
-    setPokemon((prevPokemon) => [...prevPokemon, newPokemon]);
-  };
-
-  const handleDeletePokemon = async (id) => {
-    try {
-      await axios.delete(`https://pokemon-data.adaptable.app/pokedex/${id}`);
-      setPokemon((prevPokemon) => prevPokemon.filter((pokemon) => pokemon.id !== id));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div>
       <NewPokedexEntry onAdd={handleAddPokemon} />
@@ -82,21 +48,6 @@ const MyPokedex = () => {
               <button onClick={() => handleDeletePokemon(pokemon.id)}>
                 Delete
               </button>
-            </div>
-          ))
-        ) : (
-          <p>No Pokémon</p>
-        )}
-      </div>
-      <NewPokedexEntry onAdd={handleAddPokemon} />
-
-      <div className="pokedex">
-        {pokemon ? (
-          pokemon.map((pokemon) => (
-            
-            <div key={pokemon.id} className="pokemon-entry">
-            <PokemonCard pokemon={pokemon} />
-            <button onClick={() => handleDeletePokemon(pokemon.id)}>Delete</button>
             </div>
           ))
         ) : (
