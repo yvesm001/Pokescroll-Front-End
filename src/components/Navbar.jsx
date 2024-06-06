174513eaccdb17b55d0abd39c5fb3ce0fd425d13
Victor <victorortiz928@gmail.com>
2024-06-06 10:38:41 -0400

    Make Navbar Stick on Top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <nav
-      className="navbar navbar-expand-lg navbar-dark bg-dark"
+      className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark"
       style={{ padding: "20px" }}
     >
       <NavLink className="navbar-brand" to="/">
